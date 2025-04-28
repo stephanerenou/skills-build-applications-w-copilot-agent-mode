da8922b69e8d3ecdaad963f18e2ea1bf57969d4e
Stéphane Renou <stephane.renou@visionairy.io>
2025-04-28 09:24:21 +0000

    front

---
+++ octofit-tracker/frontend/src/components/Workouts.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+function Workouts() {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://automatic-spoon-597rw4rrx7fp4px-8000.app.github.dev/api/workouts')
+      .then(response => response.json())
+      .then(data => setWorkouts(data));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="mb-4">Workouts</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {workouts.map(workout => (
+            <tr key={workout.id}>
+              <td>{workout.id}</td>
+              <td>{workout.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Workouts;
